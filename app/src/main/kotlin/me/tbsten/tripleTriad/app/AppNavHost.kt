764e1a74258a9c5131012f4c5a2222913d5e8e67
TBSten <81161390+TBSten@users.noreply.github.com>
2025-02-10 04:24:00 +0900

    アプリでゲーム画面を表示するように

--- app/src/main/kotlin/me/tbsten/tripleTriad/app/AppNavHost.kt
+++ app/src/main/kotlin/me/tbsten/tripleTriad/app/AppNavHost.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
-import me.tbsten.tripleTriad.ui.feature.example.Examples
-import me.tbsten.tripleTriad.ui.feature.example.examples
+import me.tbsten.tripleTriad.ui.feature.game.Game
+import me.tbsten.tripleTriad.ui.feature.game.game
 import me.tbsten.tripleTriad.ui.navigation.NavControllerWrapper
 
 @Composable
@@ -13,9 +13,7 @@ internal fun AppNavHost(modifier: Modifier = Modifier) {
     val navController = rememberNavController()
     val navControllerWrapper = NavControllerWrapper(navController)
 
-    NavHost(navController = navController, startDestination = Examples, modifier = modifier) {
-        examples(
-            navControllerWrapper = navControllerWrapper,
-        )
+    NavHost(navController = navController, startDestination = Game, modifier = modifier) {
+        game()
     }
 }

60aa6c9b08649b64fe0fc11a0dd2199b6434a745
TBSten <81161390+TBSten@users.noreply.github.com>
2025-02-23 00:12:29 +0900

    Remove unused import in GamePlayText.kt

--- ui/feature/game/src/main/kotlin/me/tbsten/tripleTriad/ui/feature/game/play/component/GamePlayText.kt
+++ ui/feature/game/src/main/kotlin/me/tbsten/tripleTriad/ui/feature/game/play/component/GamePlayText.kt
@@ -1,6 +1,5 @@
 package me.tbsten.tripleTriad.ui.feature.game.play.component
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
